b6af29e91b691beaa2e515cb41f920494f332495
Yacire El Rhomari <yacire.elrhomari@gmail.com>
2025-11-13 22:52:27 +0100

    chore(postgres): add postgres config

---
+++ backend/user-service/src/main/java/com/example/yass/users/model/User.java
@@ -0,0 +1,15 @@
+package com.example.yass.users.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.UUID;
+
+@Entity
+@Table(name="USERS")
+public class User {
+
+    public UUID id;
+
+    public String
+}
